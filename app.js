123abaee12bc5092eb808765ae51c523d4565e75
kane <hashka2021@gmail.com>
2022-05-29 14:18:30 +0800

    Finance modul ruu item add hiideg bolgono

--- app.js
+++ app.js
@@ -44,7 +44,29 @@ var financeController = (function(){
             inc:0,
             exp:0
         }
+    };
+    return {
+        addItem:function(type,desc,val){
+
+            var item,id;
+
+            if(data.items[type].length ===0) id=1;
+            else {
+                id = data.items[type][data.items[type].length-1].id+1;
+            }
+
+            if(type==='inc'){
+                item = new Income(id,desc,val);
+            }else{
+                item = new Expense(id,desc,val);
+            }
+            data.items[type].push(item);
+        },
+        seeData:function(){
+            return data;
     }
+    
+    };
 })();
 //Программ холбогч  контроллер
 
